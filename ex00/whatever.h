199ee60bf7d36cee734b2f2b55002b82168ec84a
Jaehyun Lee <jaehylee@student.42gyeongsan.kr>
2026-03-14 15:58:54 +0900

    :sparkles: ex00

---
+++ ex00/whatever.h
@@ -0,0 +1,14 @@
+#ifndef WHATEVER_H
+#define WHATEVER_H
+
+template <typename T> void swap(T &a, T &b) {
+  T c(a);
+  a = b;
+  b = c;
+}
+
+template <typename T> T min(T &a, T &b) { return a > b ? b : a; }
+
+template <typename T> T max(T &a, T &b) { return a < b ? b : a; }
+
+#endif
